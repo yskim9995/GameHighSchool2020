6727af04b67bee4f7fa136602baa56fec25b539e
kimyunseo <yskim9995@naver.com>
2020-07-01 19:52:54 +0900

    총알 피하기 최초 푸쉬
    Signed-off-by: kimyunseo <yskim9995@naver.com>

---
+++ Dotge game/Assets/Dodge/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float m_speed = 25;
+    // Update is called once per frame
+    void Update()
+    {
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        float xAxia = Input.GetAxis("Horizontal");
+        float yAxia = Input.GetAxis("Vertical");
+
+        rigidbody.AddForce(new Vector3(xAxia, 0, yAxia) * m_speed);
+
+
+        float fireAxis = Input.GetAxis("Fire1");
+
+        if (fireAxis > 0.95f)
+            Die();
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForce(Vector3.left * 10f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddForce(Vector3.right * 10f);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody.AddForce(Vector3.up * 10f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody.AddForce(Vector3.down * 15f);
+        }
+
+
+        if (Input.GetKey(KeyCode.Space))
+            Die();
+
+
+
+    }
+
+    public void Die()
+    {
+        Debug.Log("Die");
+        gameObject.SetActive(false);
+    }
+
+
+
+
+}
---
+++ Dotge game/Assets/Test.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Test : EditorWindow
+{
+	// Add menu named "My Window" to the Window menu
+	[MenuItem("Window/Test/Test")]
+	static void Init()
+	{
+		// Get existing open window or if none, make a new one:
+		Test window = (Test)EditorWindow.GetWindow(typeof(Test));
+		window.Show();
+	}
+
+	void OnGUI()
+	{
+		Handles.color = Color.black;
+		Handles.DrawRectangle(0, new Vector3(200, 200, 0), Quaternion.identity, 100);
+		Handles.DrawSolidDisc(new Vector3(200, 200, 0), Vector3.forward, 100);
+		Handles.DrawSolidDisc(new Vector3(150, 150, 0), Vector3.forward, 50);
+		Handles.DrawSolidDisc(new Vector3(250, 150, 0), Vector3.forward, 50);
+	}
+}
